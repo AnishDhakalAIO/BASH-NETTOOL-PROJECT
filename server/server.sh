3c8efa1cbbfe18186a1f97ad14aa5e7004b76e82
Anish <anishactitout@gmail.com>
2025-12-12 12:48:48 +0000

    uptate after testing

--- server/server.sh
+++ server/server.sh
@@ -8,6 +8,7 @@ echo " Starting simple TCP services on port $port..."
 
 # infinity loop to keep the server running 
 
+
 while true; do 
 	echo -e "Hello from Bash TCP server!\nTime: $(date)" | nc -l -p $port
 done
